38984999e22f0609c29ebf4ce3048c1073d2874c
tjmakande <50986477+tjmakande@users.noreply.github.com>
2020-08-13 17:37:39 +0800

    Update slideshow.js

--- public/js/slideshow.js
+++ public/js/slideshow.js
@@ -1,4 +1,9 @@
-$(window).scroll(function(){
-  var scrollval = $(this).scrollTop();  //this will return the scroll value
-  $(".header").css("transform", 'translate(0px,-'+scrollval*0.15+'%)');
-})
+// $(window).scroll(function(){
+//   var scrollval = $(this).scrollTop();  //this will return the scroll value
+//   $(".header").css("transform", 'translate(0px,-'+scrollval*0.15+'%)');
+// })
+
+window.onscroll = function() {
+  var scrollval = window.scrollY
+  document.querySelector('.header').style.transform = `translate(0px,-` + scrollval * 0.15+'%)';
+};
