5d4a609c70e42ad355c917d7dad987215d374121
tjmakande <50986477+tjmakande@users.noreply.github.com>
2020-08-13 16:26:15 +0800

    Update slideshow.js

--- public/js/slideshow.js
+++ public/js/slideshow.js
@@ -1,37 +1,3 @@
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-  clearStream();
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-  clearStream();
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.querySelectorAll("#mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-
-  slides[slideIndex-1].style.display = "flex";  
-
-}
-
-function clearStream(){
-  document.querySelector('.stream_details_first').style.display = 'none';
-  document.querySelector('.stream_details_second').style.display = 'none';
-  document.querySelector('.stream_details_third').style.display = 'none';
-}
-
-
 $(window).scroll(function(){
   var scrollval = $(this).scrollTop();  //this will return the scroll value
   $(".header").css("transform", 'translate(0px,-'+scrollval*0.15+'%)');
